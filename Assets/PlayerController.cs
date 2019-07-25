dd997f38dbc0b72217886d1d97c97d0f61406608
lclark <1010607@student.sae.edu.au>
2019-07-25 22:16:55 +1000

    Catchup Stuff

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //character controller setup
+    CharacterController controller;
+
+  
+    public float speed;
+
+    public float jumpSpeed;
+    public float gravity;
+
+    //value for movement direction,  .zero avoids the null value
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //obtain character controller
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if the player is on the ground
+        if (controller.isGrounded)
+        {
+            //if player is on the ground, input rotation and movement
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //move character in the chosen direction * by speed
+            moveDirection *= speed;
+
+            //player jumping function
+            if(Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        //mechanic to bring the player back to the ground
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
